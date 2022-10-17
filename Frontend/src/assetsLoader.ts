5743d7d54d7aa53e3f8f129fff9fc54fea67d426
Orlando Vargas <nojipiz@protonmail.com>
2022-10-17 15:46:41 -0500

    Add commits of dev branch

---
+++ Frontend/src/assetsLoader.ts
@@ -0,0 +1,13 @@
+import { Assets } from '@pixi/assets';
+
+// load assets
+const map = Assets.load('./assets/scenes/map.png');
+const player = Assets.load('./assets/entities/player.png');
+
+// create a list of assets
+const assets = {
+    map,
+    player,
+};
+
+export { assets };
