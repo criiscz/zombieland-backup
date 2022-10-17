361f8e41d4cbca6148b9a532c90bcad3411d66d9
Orlando Vargas <nojipiz@protonmail.com>
2022-10-17 15:30:44 -0500

    Changed position of server address

--- RealtimeBackend/src/main.rs
+++ RealtimeBackend/src/main.rs
@@ -5,10 +5,12 @@ use std::io::Error;
 mod actions;
 mod connections;
 mod domain;
+mod environment;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     env_logger::init();
-    start_server().await;
+    let server_address = "127.0.0.1:8080".to_string();
+    start_server(server_address).await;
     Ok(())
 }
