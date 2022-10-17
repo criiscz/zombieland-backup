5aad60ada961c9c6d3d39014d97aa5aa8b8e95e8
Orlando Vargas <nojipiz@protonmail.com>
2022-10-17 15:29:39 -0500

    Add modules "export"

--- RealtimeBackend/src/domain/input_message.rs
+++ RealtimeBackend/src/domain/input_message.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-use super::{events::Event, player::Player};
+use super::{event::Event, player::Player};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct InputMessage<T: Event> {
--- RealtimeBackend/src/domain/mod.rs
+++ RealtimeBackend/src/domain/mod.rs
@@ -1,3 +1,5 @@
-pub mod events;
+pub mod attack;
+pub mod enemy;
+pub mod event;
 pub mod input_message;
 pub mod player;
