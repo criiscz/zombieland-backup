6c3b4e97d7d3ef97e991bdb713d42118b73ec775
Orlando Vargas <nojipiz@protonmail.com>
2022-10-19 18:06:09 -0500

    Add action for already running players

--- RealtimeBackend/src/actions/players.rs
+++ RealtimeBackend/src/actions/players.rs
@@ -1,10 +1,14 @@
 use crate::{domain::player::Player, environment::environment::PlayersState};
 
-pub async fn handle_player(input: Player, players_state: PlayersState) {
+pub async fn handle_player(mut input: Player, players_state: PlayersState) {
     match (*players_state).lock().await {
         mut x => {
-            x.retain(|player| (*player).id != input.id);
-            x.push(input)
+            if let Some(player) = x.iter_mut().find(|player| player.id == input.id) {
+                player.update(input)
+            } else {
+                input.first_enter();
+                x.push(input);
+            };
         }
     }
 }
