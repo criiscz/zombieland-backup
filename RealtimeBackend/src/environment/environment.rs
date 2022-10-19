ae3155dceb91b92f9134a4b896c1f4548886a82b
Orlando Vargas <nojipiz@protonmail.com>
2022-10-19 09:55:47 -0500

    Created core functionalities struct

--- RealtimeBackend/src/environment/environment.rs
+++ RealtimeBackend/src/environment/environment.rs
@@ -1,20 +1,18 @@
-use futures_util::{future, pin_mut, StreamExt, TryStreamExt};
-use std::{env, io::Error, net::SocketAddr, sync::Arc, sync::Mutex, time::Duration};
-use tokio::{
-    net::{TcpListener, TcpStream},
-    time::sleep,
-};
+use std::{sync::Arc, time::Duration};
+use tokio::{sync::Mutex, time::sleep};
 
-use crate::domain::{attack::Attack, enemy::Enemy, player::Player};
+use crate::domain::{attack::Bullet, enemy::Enemy, player::Player};
 
-type Players = Arc<Mutex<Vec<Player>>>;
-type Enemies = Arc<Mutex<Vec<Enemy>>>;
-type Attacks = Arc<Mutex<Vec<Attack>>>;
+use super::{interactions::run_interactions, physics::run_physics};
+
+pub type PlayersState = Arc<Mutex<Vec<Player>>>;
+pub type EnemiesState = Arc<Mutex<Vec<Enemy>>>;
+pub type BulletsState = Arc<Mutex<Vec<Bullet>>>;
 
 pub struct Environment {
-    players: Players,
-    enemies: Enemies,
-    attacks: Attacks,
+    players: PlayersState,
+    enemies: EnemiesState,
+    attacks: BulletsState,
 }
 
 impl Environment {
@@ -26,7 +24,25 @@ impl Environment {
         }
     }
 
-    pub fn start() {
-        todo!("Make the start code!")
+    pub fn get_attacks_state(&self) -> BulletsState {
+        self.attacks.clone()
+    }
+
+    pub fn get_players_state(&self) -> PlayersState {
+        self.players.clone()
+    }
+
+    pub fn start(&self) {
+        let bullets = Arc::clone(&self.attacks);
+        let players = Arc::clone(&self.players);
+        let enemies = Arc::clone(&self.enemies);
+
+        tokio::spawn(async move {
+            loop {
+                sleep(Duration::from_millis(10)).await;
+                run_interactions(players.clone(), enemies.clone(), bullets.clone()).await;
+                run_physics(players.clone(), enemies.clone(), bullets.clone()).await;
+            }
+        });
     }
 }
