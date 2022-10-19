7d6e1d546c0772d672ea4c5d09f4bc6cb78914c6
Orlando Vargas <nojipiz@protonmail.com>
2022-10-19 09:52:50 -0500

    Add input handler for all states

--- RealtimeBackend/src/connections/player_input.rs
+++ RealtimeBackend/src/connections/player_input.rs
@@ -2,24 +2,30 @@ use serde_json::Result;
 use tokio::sync::broadcast::Receiver;
 
 use crate::{
-    actions::{attacks::handle_attacks, players::handle_events},
-    domain::{event::Bullet, input_message::InputMessage},
+    actions::{attacks::handle_attacks, players::handle_player},
+    domain::{attack::Bullet, input_message::InputMessage},
+    environment::environment::{BulletsState, PlayersState},
 };
 
-pub fn start_player_input_handler(mut receiver: Receiver<String>) {
+pub fn start_player_input_handler(
+    mut receiver: Receiver<String>,
+    players_state: PlayersState,
+    attacks_state: BulletsState,
+) {
     tokio::spawn(async move {
         while let Ok(msg) = receiver.recv().await {
-            handle_input(msg).await;
+            handle_input(msg, players_state.clone(), attacks_state.clone()).await;
         }
     });
 }
 
-async fn handle_input(input: String) {
+async fn handle_input(input: String, players_state: PlayersState, attacks_state: BulletsState) {
     let input_converted: Result<InputMessage<Bullet>> = serde_json::from_str(&input);
     match input_converted {
-        Ok(input_message) => {
-            let attacks_task = handle_attacks(&input_message);
-            let events_task = handle_events(&input_message);
+        Ok(mut input_message) => {
+            input_message.autocomplete();
+            let attacks_task = handle_attacks(input_message.attacks, attacks_state);
+            let events_task = handle_player(input_message.player, players_state);
             tokio::join!(attacks_task, events_task);
         }
         Err(error) => log::warn!("Can't deserialize the input! {}", error),
--- RealtimeBackend/src/connections/server.rs
+++ RealtimeBackend/src/connections/server.rs
@@ -1,11 +1,20 @@
+use crate::environment::environment::Environment;
+
 use super::{connections::Connections, player_input::start_player_input_handler};
 use tokio::sync::broadcast::{Receiver, Sender};
 
 pub async fn start_server(server_address: String) {
     let connections_handler = Connections::new();
+    let environment = Environment::new();
 
-    let (sender, receiver): (Sender<String>, Receiver<String>) = connections_handler.get_channels();
-    start_player_input_handler(receiver);
+    let (_sender, receiver): (Sender<String>, Receiver<String>) =
+        connections_handler.get_channels();
+
+    let attacks_state = environment.get_attacks_state();
+    let players_state = environment.get_players_state();
+
+    start_player_input_handler(receiver, players_state, attacks_state);
+    environment.start();
 
     Connections::start(&connections_handler, server_address).await;
 }
