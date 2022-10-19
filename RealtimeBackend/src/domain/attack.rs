455777e4e6ed60f49e76b3efbb25a8a04a82939d
Orlando Vargas <nojipiz@protonmail.com>
2022-10-19 09:53:37 -0500

    Add attack and enemy structures

--- RealtimeBackend/src/domain/attack.rs
+++ RealtimeBackend/src/domain/attack.rs
@@ -1,4 +1,45 @@
-pub struct Attack {
-    position_y: f32,
-    position_x: f32,
+use std::fmt::Display;
+
+use serde::{Deserialize, Serialize};
+
+pub trait Attack {
+    fn get_angle(self) -> f32;
+    fn get_type(self) -> String;
+    fn get_position(self) -> (f32, f32);
+}
+
+impl Attack for Bullet {
+    fn get_angle(self) -> f32 {
+        self.angle
+    }
+
+    fn get_type(self) -> String {
+        self.attack_type
+    }
+
+    fn get_position(self) -> (f32, f32) {
+        (self.position_x, self.position_y)
+    }
+}
+
+impl Display for Bullet {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Bullet x:{} y:{} uid:{}",
+            self.position_x, self.position_y, self.attack_type
+        )
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Bullet {
+    pub attack_type: String,
+    pub angle: f32,
+    #[serde(default)]
+    pub position_y: f32,
+    #[serde(default)]
+    pub position_x: f32,
+    #[serde(default)]
+    pub player_id: u16,
 }
--- RealtimeBackend/src/domain/enemy.rs
+++ RealtimeBackend/src/domain/enemy.rs
@@ -1,6 +1,6 @@
 pub struct Enemy {
-    position_y: f32,
-    position_x: f32,
-    skin: u8,
-    axis: u8,
+    pub position_y: f32,
+    pub position_x: f32,
+    pub skin: u8,
+    pub axis: u8,
 }
