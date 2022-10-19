f9f09503b8ca518b45a0055084cd27737d99c1cf
Orlando Vargas <nojipiz@protonmail.com>
2022-10-19 09:55:21 -0500

    Add input structure for deserialize information

--- RealtimeBackend/src/domain/input_message.rs
+++ RealtimeBackend/src/domain/input_message.rs
@@ -1,9 +1,22 @@
 use serde::{Deserialize, Serialize};
 
-use super::{event::Event, player::Player};
+use super::{
+    attack::{Attack, Bullet},
+    player::Player,
+};
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct InputMessage<T: Event> {
-    player: Player,
-    events: Vec<T>,
+pub struct InputMessage<T: Attack> {
+    pub player: Player,
+    pub attacks: Vec<T>,
+}
+
+impl InputMessage<Bullet> {
+    pub fn autocomplete(&mut self) {
+        self.attacks.iter_mut().for_each(|attack| {
+            attack.position_y = self.player.position_y;
+            attack.position_x = self.player.position_x;
+            attack.player_id = self.player.id;
+        });
+    }
 }
