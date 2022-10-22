48f6fa1162ed6d3ec4f022462c81fd4bba29355a
Orlando Vargas <nojipiz@protonmail.com>
2022-10-22 09:14:29 -0500

    Identation change

--- Frontend/src/types.d.ts
+++ Frontend/src/types.d.ts
@@ -1,20 +1,20 @@
-import { Texture } from "pixi.js";
+import { Texture } from 'pixi.js';
 
 interface SpritesList {
-  [key: string]: Texture | undefined;
+    [key: string]: Texture | undefined;
 }
 
 interface IPlayer {
-  id: string;
-  name: string;
-  x: number;
-  y: number;
-  axis: number;
-  speed: number;
-  hp: number;
-  maxHp: number;
-  score: number;
-  isDead: boolean;
+    id: number;
+    name: string;
+    x: number;
+    y: number;
+    axis: number;
+    speed: number;
+    hp: number;
+    maxHp: number;
+    score: number;
+    isDead: boolean;
 }
 
 export { SpritesList, IPlayer };
