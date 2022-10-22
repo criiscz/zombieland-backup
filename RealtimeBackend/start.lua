535c78e2573d2e6f9f18cd712742d731b1f136dd
Orlando Vargas <nojipiz@protonmail.com>
2022-10-22 13:04:30 -0500

    Container building almost everything

--- RealtimeBackend/start.lua
+++ RealtimeBackend/start.lua
@@ -16,8 +16,10 @@ end
 -- Start of script
 
 local operating_system = OS()
-local networkname = "zombieland-network"
-local container_df_name = "dragonflydb"
+local networkname = "zombieland_network"
+local dragonfly_container = "dragonflydb"
+local realtime_image = "zombieland_realtime"
+local realtime_container = "zombieland_rust_container"
 local prefix = ""
 
 local function create_virtual_network()
@@ -28,13 +30,13 @@ end
 local function create_dragonfly_container()
   print("Creating dragonflydb container")
   os.capture(prefix ..
-    "docker run --network=" ..
+    "docker run -d --network=" ..
     networkname .. " --ulimit memlock=-1 --name " ..
-    container_df_name .. "docker.dragonflydb.io/dragonflydb/dragonfly")
+    dragonfly_container .. " docker.dragonflydb.io/dragonflydb/dragonfly")
 end
 
 local function check_os()
-  if (operating_system == "unix") then
+  if operating_system == "unix" then
     prefix = "sudo "
     print("Executing in linux ...")
   else
@@ -44,7 +46,7 @@ end
 
 local function check_network()
   local created_networks = os.capture(prefix .. "docker network ls")
-  if (string.find(created_networks, networkname)) then
+  if string.find(created_networks, networkname) ~= nil then
     print("Already created zombieland-network in docker")
   else
     create_virtual_network()
@@ -53,7 +55,7 @@ end
 
 local function check_containers()
   local created_containers = os.capture(prefix .. "docker container ls -a")
-  if (string.find(created_containers, container_df_name)) then
+  if string.find(created_containers, dragonfly_container) then
     print("Already created dragonflydb container")
   else
     create_dragonfly_container()
@@ -61,9 +63,9 @@ local function check_containers()
 end
 
 local function start_df_container()
-  local dragonfly_start = os.capture(prefix .. "docker start " .. container_df_name)
-  if (string.find(dragonfly_start, container_df_name)) then
-    print("Dragonfly container executing...")
+  local dragonfly_start = os.capture(prefix .. "docker start " .. dragonfly_container)
+  if string.find(dragonfly_start, dragonfly_container) then
+    print("Dragonfly container executing in background...")
     return true
   else
     print("Can't start dragonfly container, stopping execution")
@@ -72,6 +74,13 @@ local function start_df_container()
 end
 
 local function start_rust_container()
+  print("Building Rust image (This may take up a while)...")
+  os.capture(prefix .. "docker build -t " .. realtime_image .. " . ")
+  print("Rust container executing in background...")
+  os.capture(prefix ..
+    "docker run --network=" .. networkname .. " -d -it -p 8080:8080 --name=" ..
+    realtime_container .. " " .. realtime_image
+  )
 end
 
 local function start()
@@ -79,7 +88,13 @@ local function start()
   check_network()
   check_containers()
   local df_start = start_df_container()
+  if (df_start == false) then
+    return print("Failed to start Dragonfly container")
+  end
   local rust_start = start_rust_container()
+  if (rust_start == false) then
+    return print("Failed to start Rust container")
+  end
 end
 
 start()
