cd78cac92b0de80095906f369db8b77a4d905d59
Orlando Vargas <nojipiz@protonmail.com>
2022-10-22 22:39:05 -0500

    Add disconnection handling

--- RealtimeBackend/src/actions/players.rs
+++ RealtimeBackend/src/actions/players.rs
@@ -1,6 +1,6 @@
 use crate::{domain::player::Player, environment::environment::PlayersState};
 
-pub async fn handle_player(mut input: Player, players_state: PlayersState) {
+pub async fn handle_player_update(mut input: Player, players_state: PlayersState) {
     match (*players_state).lock().await {
         mut x => {
             if let Some(player) = x.iter_mut().find(|player| player.id == input.id) {
@@ -11,4 +11,11 @@ pub async fn handle_player(mut input: Player, players_state: PlayersState) {
             };
         }
     }
+}
+
+pub async fn handle_player_disconnection(player_address: String, players_state: PlayersState) {
+    (*players_state)
+        .lock()
+        .await
+        .retain(|player| player.connection_address != player_address)
 }
