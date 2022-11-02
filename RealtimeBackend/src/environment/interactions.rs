d54a324e5b07d2238c1e471b9671bb13baf5d7ad
Orlando Vargas <nojipiz@protonmail.com>
2022-11-02 12:24:01 -0500

    refactor: Converted to module with implementation

--- RealtimeBackend/src/environment/interactions.rs
+++ RealtimeBackend/src/environment/interactions.rs
@@ -11,15 +11,26 @@ use crate::{
 };
 use futures_util::{future::join_all, stream::FuturesUnordered};
 
-pub async fn run_interactions(players: PlayersState, enemies: EnemiesState, bullets: BulletsState) {
-    let interactions: Vec<Box<dyn Interaction + Sync + Send>> = vec![
-        Box::new(DamageToPlayersByEnemies::new(players, enemies.clone())),
-        Box::new(DamageToEnemiesByBullets::new(enemies, bullets)),
-    ];
+pub struct InteractionsModule {
+    interactions: Vec<Box<dyn Interaction + Sync + Send>>,
+}
 
-    let tasks = interactions
-        .into_iter()
-        .map(|interaction| tokio::spawn(async move { interaction.run().await }))
-        .collect::<FuturesUnordered<_>>();
-    join_all(tasks).await;
+impl InteractionsModule {
+    pub fn new(players: PlayersState, enemies: EnemiesState, bullets: BulletsState) -> Self {
+        InteractionsModule {
+            interactions: vec![
+                Box::new(DamageToPlayersByEnemies::new(players, enemies.clone())),
+                Box::new(DamageToEnemiesByBullets::new(enemies, bullets)),
+            ],
+        }
+    }
+
+    pub async fn run(&self) {
+        let tasks = self
+            .interactions
+            .iter()
+            .map(|interaction| interaction.run())
+            .collect::<FuturesUnordered<_>>();
+        join_all(tasks).await;
+    }
 }
--- RealtimeBackend/src/environment/physics.rs
+++ RealtimeBackend/src/environment/physics.rs
@@ -2,21 +2,29 @@ mod bullets_physics;
 mod enemies_physics;
 mod physic;
 
+use self::{bullets_physics::BulletsPhysics, enemies_physics::EnemiesPhysics, physic::Physic};
+use crate::domain::state_types::{BulletsState, EnemiesState, PlayersState};
 use futures_util::{future::join_all, stream::FuturesUnordered};
 
-use crate::domain::state_types::{BulletsState, EnemiesState, PlayersState};
+pub struct PhysicsModule {
+    physics: Vec<Box<dyn Physic + Sync + Send>>,
+}
 
-use self::{bullets_physics::BulletsPhysics, enemies_physics::EnemiesPhysics, physic::Physic};
+impl PhysicsModule {
+    pub fn new(players: PlayersState, enemies: EnemiesState, bullets: BulletsState) -> Self {
+        let physics: Vec<Box<dyn Physic + Sync + Send>> = vec![
+            Box::new(BulletsPhysics::new(bullets)),
+            Box::new(EnemiesPhysics::new(enemies, players)),
+        ];
+        PhysicsModule { physics }
+    }
 
-pub async fn run_physics(players: PlayersState, enemies: EnemiesState, bullets: BulletsState) {
-    let physics: Vec<Box<dyn Physic + Sync + Send>> = vec![
-        Box::new(BulletsPhysics::new(bullets)),
-        Box::new(EnemiesPhysics::new(enemies, players)),
-    ];
-
-    let tasks = physics
-        .into_iter()
-        .map(|physic| tokio::spawn(async move { physic.run().await }))
-        .collect::<FuturesUnordered<_>>();
-    join_all(tasks).await;
+    pub async fn run(&self) {
+        let tasks = self
+            .physics
+            .iter()
+            .map(|physic| physic.run())
+            .collect::<FuturesUnordered<_>>();
+        join_all(tasks).await;
+    }
 }
