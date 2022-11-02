3b63ce1385d276b17424a46a196d5545828a400b
Orlando Vargas <nojipiz@protonmail.com>
2022-11-02 13:00:36 -0500

    refactor: Add Gamestate wrapper

--- RealtimeBackend/src/domain/output_message.rs
+++ RealtimeBackend/src/domain/output_message.rs
@@ -1,10 +1,20 @@
 use serde::{Deserialize, Serialize};
 
-use super::{attack::Bullet, enemy::Enemy, player::Player};
+use super::{attack::Bullet, enemy::Enemy, player::Player, state_types::GameState};
 
 #[derive(Serialize, Deserialize)]
 pub struct OutputMessage {
-    pub players: Vec<Player>,
-    pub enemies: Vec<Enemy>,
-    pub bullets: Vec<Bullet>,
+    players: Vec<Player>,
+    enemies: Vec<Enemy>,
+    bullets: Vec<Bullet>,
+}
+
+impl OutputMessage {
+    pub async fn from(game_state: GameState) -> Self {
+        OutputMessage {
+            players: game_state.players.lock().await.to_owned(),
+            enemies: game_state.enemies.lock().await.to_owned(),
+            bullets: game_state.bullets.lock().await.to_owned(),
+        }
+    }
 }
--- RealtimeBackend/src/domain/state_types.rs
+++ RealtimeBackend/src/domain/state_types.rs
@@ -7,3 +7,10 @@ use super::{attack::Bullet, enemy::Enemy, player::Player};
 pub type PlayersState = Arc<Mutex<Vec<Player>>>;
 pub type EnemiesState = Arc<Mutex<Vec<Enemy>>>;
 pub type BulletsState = Arc<Mutex<Vec<Bullet>>>;
+
+#[derive(Clone)]
+pub struct GameState {
+    pub players: PlayersState,
+    pub enemies: EnemiesState,
+    pub bullets: BulletsState,
+}
